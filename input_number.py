d5870583dbca51759f79a2d717631af3450abff7
Xyrene Jade Torralba <182231457+xytorralba@users.noreply.github.com>
2024-11-01 21:37:17 +0800

    The user can now input numbers between 1 and 50.

--- input_number.py
+++ input_number.py
@@ -6,9 +6,16 @@ num31_to_40 = 0
 num41_to_50 = 0
 
 #Loop: asks user to input numbers from 1 to 50
-#Counts number of input between 1 to 10
-#Counts number of input between 11 to 20
-#Counts number of input between 21 to 30
-#Counts number of input between 31 to 40
-#Counts number of input between 41 to 50
-#Stop the loop if input is invalid
+while True: 
+    try:
+        number = int(input("Input a number from 1 to 50: "))
+        #Counts number of input between 1 to 10
+        #Counts number of input between 11 to 20
+        #Counts number of input between 21 to 30
+        #Counts number of input between 31 to 40
+        #Counts number of input between 41 to 50
+        break
+
+    #Stop the loop if input is invalid
+    except:
+        break
