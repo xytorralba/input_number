9e4c7dcb9a7a076a4648c94aa32a4c08975b0d2a
Xyrene Jade Torralba <182231457+xytorralba@users.noreply.github.com>
2024-11-01 21:09:00 +0800

    The pseudocode is created.

---
+++ input_number.py
@@ -0,0 +1,7 @@
+#Loop: asks user to input numbers from 1 to 50
+#Counts number of input between 1 to 10
+#Counts number of input between 11 to 20
+#Counts number of input between 21 to 30
+#Counts number of input between 31 to 40
+#Counts number of input between 41 to 50
+#Stop the loop if input is invalid
